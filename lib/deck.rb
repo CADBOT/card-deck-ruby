2e18198087588648af35a7538b786bc52610029b
Crawford III <icharles@amazon.com>
2014-05-07 02:29:37 -0700

    Rename test file. Extract valid suits from the deck and cards class to a module

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,22 +1,22 @@
+module Suits
+  Allowed_suits = [:hearts, :clubs, :diamonds, :spades]
+end
+
 class Card
   attr_reader :rank, :suit
-  @@allowed_suits = [:hearts, :clubs, :diamonds, :spades]
-
   def initialize(rank, suit)
     @rank = rank
-    raise unless  @@allowed_suits.include?(suit)
+    raise unless Suits::Allowed_suits.include?(suit)
     @suit = suit 
   end
-  
 end
 
 class Deck
-  @@allowed_suits = [:hearts, :clubs, :diamonds, :spades]
   def initialize
     @cards = []
     52.times do
       rank = rand(0..13)
-      suit = @@allowed_suits[rand(0..3)]
+      suit = Suits::Allowed_suits[rand(0..3)]
       @cards << Card.new(rank, suit)
     end 
   end
